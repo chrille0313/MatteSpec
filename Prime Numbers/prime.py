5cc7af4505a60259b3b5e3a71cf08f7c0534d641
Christian <chrille_0313@hotmail.com>
2022-03-25 08:47:55 +0100

    Create prime.py

---
+++ Prime Numbers/prime.py
@@ -0,0 +1,40 @@
+from Timing.timing import timing
+
+
+@timing
+def primes(n):
+	isPrime = [True] * (n + 1)
+	isPrime[0] = False
+	isPrime[1] = False
+
+	i = 2
+	while i * i <= n:
+		if isPrime[i]:
+			j = 2 * i
+			while j <= n:
+				isPrime[j] = False
+				j += i
+
+		i += 1
+
+	return isPrime
+
+
+@timing
+def is_prime(n):
+	if n == 2:
+		return True
+	elif n & 1 == 0 or n < 2:
+		return False
+
+	d = 3
+	while d * d <= n:
+		if n % d == 0:
+			return False
+		d += 2
+
+	return True
+
+
+# primes(3314192745)
+# print(is_prime(30313))
