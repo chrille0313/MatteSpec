304cdb3611c30592bc0583bbf91068183f47761c
Christian <chrille_0313@hotmail.com>
2022-03-29 19:09:08 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,17 @@ from Fractions.fractions import Fraction
 fraction1 = Fraction(0.25)
 fraction2 = Fraction(0.2)
 
-print(fraction1)
-print(fraction2)
-print(fraction1 * fraction2)
-print(fraction1 / fraction2)
-print(-fraction1)
+# print(fraction1, fraction2)
+# print(-fraction1, -fraction2)
+
+# print(fraction1 - 3)
+# print(3 - fraction1)
+
+# print(fraction1 + 2)
+# print(2 + fraction1)
+
+# print(fraction1 * fraction2)
+# print(fraction2 * fraction1)
+
+# print(fraction1 / fraction2)
+# print(fraction2 / fraction1)
